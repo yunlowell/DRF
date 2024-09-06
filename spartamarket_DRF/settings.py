7b4d97fa525896b5bfb154c64469432b07bb1118
yunlowell <dbsdbf95@gmail.com>
2024-09-06 18:15:46 +0900

    restframework 설치

--- spartamarket_DRF/settings.py
+++ spartamarket_DRF/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party
+    'rest_framework',
+
+    # local
     'produscts',
     'accounts',
     'users',
@@ -101,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ]
+}
 
 
 # Internationalization
