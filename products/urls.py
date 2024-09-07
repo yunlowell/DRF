99ad6112456bbeff5726c29b7a4bb2ffb6758c05
yunlowell <dbsdbf95@gmail.com>
2024-09-07 21:43:10 +0900

    feat: 글 상세조회 기능 추가

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "products"
 urlpatterns = [
     path("", views.article_list, name="article_list"),
+    path("<int:pk>/", views.article_detail, name="article_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import Article
 from .serializers import ArticleSerializer
 
@@ -10,4 +11,11 @@ from .serializers import ArticleSerializer
 def article_list(request):
     articles = Article.objects.all()
     serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data)
+
+# 글 상세 조회
+@api_view(["GET"])
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    serializer = ArticleSerializer(article)
     return Response(serializer.data)
