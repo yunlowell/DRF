96ec3158bf0330ec18ce2d148746998865ecf021
yunlowell <dbsdbf95@gmail.com>
2024-09-07 22:15:41 +0900

    feat: 글 삭제 기능 추가

--- products/views.py
+++ products/views.py
@@ -21,9 +21,15 @@ def article_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-# 글 상세 조회
-@api_view(["GET"])
+# 글 상세 조회, 삭제
+@api_view(["GET", "DELETE"])
 def article_detail(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    serializer = ArticleSerializer(article)
-    return Response(serializer.data)
+    if request.method == "GET":
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    elif request.method == "DELETE":
+        article = get_object_or_404(Article, pk=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
