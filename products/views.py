b4d018c5bee07d774075f083c149c2188250eef0
yunlowell <dbsdbf95@gmail.com>
2024-09-07 22:10:20 +0900

    feat: 글 생성 기능 추가

--- products/views.py
+++ products/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Article
 from .serializers import ArticleSerializer
 
 # Create your views here.
-# 글 목록 조회
-@api_view(["GET"])
+# 글 목록 조회, 글 생성
+@api_view(["GET", "POST"])
 def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data)
-
+    if request.method == "GET":
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
 # 글 상세 조회
 @api_view(["GET"])
 def article_detail(request, pk):
