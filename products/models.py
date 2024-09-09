b09554772a2f6ebc4e06524fe902f17d00b35dc7
yunlowell <dbsdbf95@gmail.com>
2024-09-09 20:15:00 +0900

    feat: 상품 등록시 이미지 파일 업로드 기능

--- products/models.py
+++ products/models.py
@@ -6,5 +6,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    image = models.ImageField(upload_to='products/', null=True, blank=True) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- products/serializers.py
+++ products/serializers.py
@@ -5,3 +5,10 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = "__all__"
+
+    def validate(self, data):
+        if not data.get('title') or not data.get('content'):
+            raise serializers.ValidationError("제목과 내용을 입력하세요.")
+        if not data.get('image'):
+            raise serializers.ValidationError("상품 이미지를 첨부하세요.")
+        return data
